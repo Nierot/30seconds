f10398fe15a66e3827fad68d80ecedb2310e951b
Niels <nielsrotmensen@outlook.com>
2019-12-25 10:00:10 +0100

    Made the randomInt() function work properly

--- game/utils.py
+++ game/utils.py
@@ -16,6 +16,22 @@ def xAppeared(operator):
                 most = i.times_appeared
         return most
 
+def randomBoolean():
+    return random.randint(0,99) < 50
+
+
+def randomInt():
+    wordList = Word.objects.all()
+    max = Word.objects.all().order_by("-id")[0].id - 1
+    half = round((xAppeared(">") - xAppeared("<")) / 2)
+    while True:
+        randID = random.randint(0,max)
+        word = wordList[randID]
+        if randomBoolean():
+            return randID
+        elif word.times_appeared < half:
+            return randID
+
 
 def kindaRandomInt():
     max = Word.objects.all().order_by("-id")[0].id - 1
@@ -25,19 +41,24 @@ def kindaRandomInt():
     half = round((most - least) / 2)
     i = 0
     counter = 0
-    while i < 5:
+    while len(returnList) != 5:
         randId = random.randint(0, max)
-        word = Word.objects.all()[randId]
         if randId not in returnList:
-            rareness = word.times_appeared
-            print(rareness)
-            print(half)
-            if rareness <= half:
+            randFactor = random.randint(0, 100)
+            if randFactor < 50:
                 returnList.append(randId)
-                i += 1
             else:
-                counter += 1
-            if counter == 10:
-                half += 1
-                counter = 0
+                j = 0
+                while j != 1:
+                    randId = random.randint(0, max)
+                    word = Word.objects.all()[randId]
+                    rareness = word.times_appeared
+                    if rareness <= half:
+                        returnList.append(randId)
+                        j = 1
+                    else:
+                        counter += 1
+                    if counter == 10:
+                        half += 1
+                        counter = 0
     return returnList
--- game/views.py
+++ game/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 import random
-from game.utils import kindaRandomInt
+from game.utils import kindaRandomInt, randomInt
 
 from .models import Word, Category
 
@@ -10,20 +10,24 @@ from .models import Word, Category
 def index(request):
     wordList = Word.objects.all()
     returnList = []
-    ids = kindaRandomInt()
+    ids = []
+    while len(ids) < 5:
+        foo = randomInt()
+        if len(ids) < 5:
+            randInt = randomInt()
+            if randInt not in ids:
+                ids.append(randInt)
     print(ids)
-    i = 0 
+    i = 0
     while i < 5:
-        for i in ids:
-            word = wordList[i]
+        for j in ids:
+            word = wordList[j]
+            print(word)
             if word not in returnList:
                 returnList.append(word)
-                returnList.append(word.times_appeared)
                 word.times_appeared += 1
                 word.save()
-                print(word)
                 i += 1
-                print(returnList)
     return render(request, 'game/words.html', {'returnList': returnList}) 
     """
     max = Word.objects.all().order_by("-id")[0].id - 1
